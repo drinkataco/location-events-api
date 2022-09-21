bcdf10329ef7a092be2dfb84371ab4dc1dba7533
J J Walwyn <joshwalwyn@economist.com>
2022-09-21 11:38:01 +0100

    Add support for LOG_LEVEL

--- src/consts.ts
+++ src/consts.ts
@@ -5,6 +5,9 @@ dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV;
 
+// Default pino log level. @see https://github.com/pinojs/pino/blob/master/docs/api.md#level-string
+export const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+
 // Fastify Server Config (https://www.fastify.io/docs/latest/Reference/Server/)
 export const SERVER_PORT = process.env.SERVER_PORT || 3000;
 export const SERVER_HOST = process.env.SERVER_HOST || '127.0.0.1';
--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,11 @@
 import pino from 'pino';
 
-import { NODE_ENV } from './consts';
+import { NODE_ENV, LOG_LEVEL } from './consts';
 
 export default (): pino.Logger => {
-  const loggerConfig: pino.LoggerOptions = {};
+  const loggerConfig: pino.LoggerOptions = {
+    level: LOG_LEVEL,
+  };
 
   if (NODE_ENV === 'local') {
     loggerConfig.transport = {
