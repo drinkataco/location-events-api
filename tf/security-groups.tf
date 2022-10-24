f61eca592f2a61e6178e427ba6357b0c7e8b0dc9
J J Walwyn <joshwalwyn@economist.com>
2022-10-24 21:28:06 +0100

    Add EKS Module

--- tf/security-groups.tf
+++ tf/security-groups.tf
@@ -1,3 +1,6 @@
+#
+# HTTP(S) SG
+#
 resource "aws_security_group" "main" {
   name = "${var.env_name}-main-sg"
 
@@ -38,4 +41,36 @@ resource "aws_security_group" "main" {
   }
 
   tags = var.aws_resource_tags
+}
+
+#
+# EKS SGs
+#
+resource "aws_security_group" "eks" {
+  name = "${var.env_name}-eks-sg"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "All (for now)"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
