46fdb20b821ba313283acf11ec65e2d713338fee
J J Walwyn <joshwalwyn@economist.com>
2022-10-28 15:39:50 +0100

    Add Init Kubernetes Resources

--- tf/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
