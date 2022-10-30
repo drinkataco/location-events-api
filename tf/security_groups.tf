99b768ff66217e11837660ce14379f877b24cc9f
J J Walwyn <joshwalwyn@economist.com>
2022-10-30 16:02:50 +0000

    Add inbound HTTP(S) rules to default ingress

--- tf/security_groups.tf
+++ tf/security_groups.tf
@@ -4,6 +4,25 @@ resource "aws_security_group" "eks" {
   vpc_id = aws_vpc.main.id
 
   #
+  # HTTP(S)
+  #
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 433
+    protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #
   # Egress to Mongo DB
   #
   egress {
